dbaed1916beac5f89dd6b82a0518fde9c1aec2dc
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-05 01:17:26 +1000

    publish v0.0.1

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! ([![Build Status](https://travis-ci.com/kurtlawrence/cmdr.svg?branch=master)](https://travis-ci.com/kurtlawrence/cmdr)
+//! [![Latest Version](https://img.shields.io/crates/v/cmdr.svg)](https://crates.io/crates/cmdr) 
+//! [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cmdr) 
+//! [![codecov](https://codecov.io/gh/kurtlawrence/cmdr/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cmdr)
+//! 
+//! (Rust) commands tree.
+//! 
+//! See the [rs docs](https://docs.rs/cmdr/).
+//! Look at progress and contribute on [github.](https://github.com/kurtlawrence/cmdr)
+//! 
+//! Currently WIP placeholder.
