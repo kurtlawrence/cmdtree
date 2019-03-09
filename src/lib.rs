10b89719ae9f970f1d64ad734e9600acc26a9bd2
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-10 06:44:58 +1000

    started parse line

--- src/lib.rs
+++ src/lib.rs
@@ -22,101 +22,101 @@ mod builder;
 mod parse;
 
 use self::parse::LineResult;
-pub use builder::Builder;
+pub use builder::{Builder, BuilderChain};
 
 pub struct Commander<'r> {
-	root: Rc<SubClass<'r>>,
-	current: Rc<SubClass<'r>>,
+    root: Rc<SubClass<'r>>,
+    current: Rc<SubClass<'r>>,
 }
 
 impl<'r> Commander<'r> {
-	pub fn prompt(&self) -> &str {
-		&self.current.name
-	}
+    pub fn prompt(&self) -> &str {
+        &self.current.name
+    }
 
-	pub fn run(mut self) {
-		let interface = Interface::new("commander").expect("failed to start interface");
-		let mut exit = false;
+    pub fn run(mut self) {
+        let interface = Interface::new("commander").expect("failed to start interface");
+        let mut exit = false;
 
-		while !exit {
-			interface
-				.set_prompt(&format!("{}=> ", self.prompt().bright_cyan()))
-				.expect("failed to set prompt");
+        while !exit {
+            interface
+                .set_prompt(&format!("{}=> ", self.prompt().bright_cyan()))
+                .expect("failed to set prompt");
 
-			match interface.read_line() {
-				Ok(ReadResult::Input(s)) => match self.parse_line(&s, true, &mut std::io::stdout())
-				{
-					LineResult::Exit => exit = true,
-					_ => (),
-				},
-				_ => (),
-			}
-		}
-	}
+            match interface.read_line() {
+                Ok(ReadResult::Input(s)) => match self.parse_line(&s, true, &mut std::io::stdout())
+                {
+                    LineResult::Exit => exit = true,
+                    _ => (),
+                },
+                _ => (),
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
-pub struct SubClass<'a> {
-	name: String,
-	help: &'a str,
-	classes: Vec<Rc<SubClass<'a>>>,
-	actions: Vec<Action<'a>>,
+struct SubClass<'a> {
+    name: String,
+    help: &'a str,
+    classes: Vec<Rc<SubClass<'a>>>,
+    actions: Vec<Action<'a>>,
 }
 
 impl<'a> SubClass<'a> {
-	fn with_name(name: &str, help_msg: &'a str) -> Self {
-		SubClass {
-			name: name.to_lowercase(),
-			help: help_msg,
-			classes: Vec::new(),
-			actions: Vec::new(),
-		}
-	}
+    fn with_name(name: &str, help_msg: &'a str) -> Self {
+        SubClass {
+            name: name.to_lowercase(),
+            help: help_msg,
+            classes: Vec::new(),
+            actions: Vec::new(),
+        }
+    }
 }
 
 struct Action<'a> {
-	name: String,
-	help: &'a str,
-	closure: RefCell<Box<FnMut(&[&str]) + 'a>>,
+    name: String,
+    help: &'a str,
+    closure: RefCell<Box<FnMut(&[&str]) + 'a>>,
 }
 
 impl<'a> Action<'a> {
-	fn call(&self, arguments: &[&str]) {
-		let c = &mut *self.closure.borrow_mut();
-		c(arguments);
-	}
+    fn call(&self, arguments: &[&str]) {
+        let c = &mut *self.closure.borrow_mut();
+        c(arguments);
+    }
 
-	#[cfg(test)]
-	fn blank_fn(name: &str, help_msg: &'a str) -> Self {
-		Action {
-				name: name.to_lowercase(),
-				help: help_msg,
-				closure: RefCell::new(Box::new(|_| ())),
-			}
-	}
+    #[cfg(test)]
+    fn blank_fn(name: &str, help_msg: &'a str) -> Self {
+        Action {
+            name: name.to_lowercase(),
+            help: help_msg,
+            closure: RefCell::new(Box::new(|_| ())),
+        }
+    }
 }
 
 impl<'a> PartialEq for Action<'a> {
-	fn eq(&self, other: &Self) -> bool {
-		self.name == other.name && self.help == other.help
-	}
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name && self.help == other.help
+    }
 }
 
 impl<'a> fmt::Debug for Action<'a> {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "Action {{ name: {}, help: {} }}", self.name, self.help)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Action {{ name: {}, help: {} }}", self.name, self.help)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn subclass_with_name_test() {
-		let sc = SubClass::with_name("NAME", "Help Message");
-		assert_eq!(&sc.name, "name");
-		assert_eq!(sc.help, "Help Message");
-	}
+    #[test]
+    fn subclass_with_name_test() {
+        let sc = SubClass::with_name("NAME", "Help Message");
+        assert_eq!(&sc.name, "name");
+        assert_eq!(sc.help, "Help Message");
+    }
 
 }
